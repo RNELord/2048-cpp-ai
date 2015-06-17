149af67e847a67571158efcddbdfc5c80263eb30
Ryan Lord <rnelord@users.noreply.github.com>
2015-06-17 22:00:09 +0200

    Initial Commit

---
+++ 2048-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2048-cpp
+//
+//  Created by Ryan Lord on 17/06/15.
+//  Copyright (c) 2015 Ryan Lord. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
